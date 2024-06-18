3106255f750e4d847d8e17f745e7c42d39e105c2
eunjeeing <ejjeongggg@gmail.com>
2024-06-18 21:53:53 +0900

    Feat: 메인페이지 컴포넌트 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import Banner from '../components/common/Banner';
 import EventBanner from '../components/common/EventBanner';
 import React from 'react';
 import MainLayout from '../layout/MainLayout';
+import DayCalendar from '../components/common/DayCalendar';
 
 function MainPage() {
   return (
@@ -13,6 +14,7 @@ function MainPage() {
       <article>
         <EventBanner title={'🔥이번 주 핫한 곳은?🔥'} />
         <EventBanner title={'OO님의 취향 저격 행사✨🔫'} />
+        <DayCalendar />
       </article>
     </div>
   );
