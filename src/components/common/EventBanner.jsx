7d80bf032b74d48b7d8f943aa0e2ec08edade665
eunjeeing <ejjeongggg@gmail.com>
2024-06-19 09:38:51 +0900

    Feat: 배너 컴포넌트 수정

--- src/components/common/EventBanner.jsx
+++ src/components/common/EventBanner.jsx
@@ -1,18 +1,160 @@
+import '../../assets/scss/common.scss';
 import React from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Link } from 'react-router-dom';
 import image from '../../assets/img/event/eventImg.jpeg';
-import favoriteImage from '../../assets/img/layout/bookmark.svg';
-import afterImage from '../../assets/img/layout/download.png';
+import favoriteImage from '../../assets/img/common/bookmark.svg';
+import afterImage from '../../assets/img/common/background.png';
 import { useState, useRef, useEffect, useCallback } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
 
+const BannerWrap = styled.div`
+  width: 90%;
+  margin: 20px auto 0;
+`;
+
+const Header = styled.header`
+  font-family: '해피니스 산스 타이틀';
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  justify-content: space-between;
+  padding: 0 20px;
+  box-sizing: border-box;
+`;
+
+const Title = styled.h2`
+  display: block;
+  font-size: 1.5em;
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+  unicode-bidi: isolate;
+`;
+
+const MoreView = styled.div`
+  font-family: '해피니스 산스';
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  padding: 15px 20px 10px;
+`;
+
+const EventInner = styled.div`
+  width: 100%;
+  max-width: 960px;
+  height: -moz-fit-content;
+  height: fit-content;
+  padding-left: 20px;
+`;
+
+const PopupBannerList = styled.div`
+  width: fit-content !important;
+  height: 100%;
+  position: relative;
+  cursor: pointer;
+`;
+
+const FavoriteP = styled.p`
+  display: block;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  unicode-bidi: isolate;
+`;
+
+const FavoriteA = styled.a`
+  position: absolute;
+  color: inherit;
+  cursor: pointer;
+  right: 0;
+  top: 0;
+`;
+
+const FavoriteImg = styled.img`
+  overflow-clip-margin: content-box;
+  overflow: clip;
+  display: block;
+`;
+
+const SlideImgWrap = styled.div`
+  display: block;
+  unicode-bidi: isolate;
+  box-sizing: border-box;
+
+  &:after {
+    background: url(${afterImage}) 0 0;
+    content: ' ';
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background-size: cover;
+    left: 0;
+    top: 0;
+    opacity: 0.8;
+    z-index: 1;
+  }
+`;
+
+const EventImage = styled.img`
+  width: 260px;
+  height: 260px;
+  border-radius: 8px;
+`;
+
+const EventInfo = styled.ul`
+  position: absolute;
+  left: -25px;
+  bottom: 20px;
+  z-index: 10;
+  color: white;
+`;
+
+const EventLi = styled.li`
+  padding-bottom: 5px;
+  list-style: none;
+`;
+
+const EventName = styled(EventLi)`
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const EventBranch = styled(EventLi)`
+  font-size: 12px;
+  opacity: 0.6;
+  font-weight: 600;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 3px;
+`;
+
+const EventType = styled(EventLi)`
+  font-size: 10px;
+  opacity: 0.6;
+  font-weight: 600;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  column-gap: 3px;
+`;
+
 function EventBanner({ title, type }) {
+  const [data, setData] = useState([]);
+
   const [dragging, setDragging] = useState(false);
   const handleBeforeChange = useCallback(() => {
     setDragging(true);
@@ -46,6 +188,21 @@ function EventBanner({ title, type }) {
     afterChange: handleAfterChange,
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/event/list/${type}?memberId=1`);
+        const result = response.data.eventList;
+        setData(result);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [type]);
+
   //bookmark src 변경
   //   const [activeIndex, setActiveIndex] = useState(null);
 
@@ -62,145 +219,6 @@ function EventBanner({ title, type }) {
   //     }
   //   };
 
-  const BannerWrap = styled.div`
-    width: 100%;
-    max-width: 960px;
-    margin: 20px auto 0;
-  `;
-
-  const Header = styled.header`
-    font-family: '해피니스 산스 타이틀';
-    width: 100%;
-    height: auto;
-    display: flex;
-    flex-direction: row;
-    align-items: baseline;
-    justify-content: space-between;
-    padding: 0 20px;
-    box-sizing: border-box;
-  `;
-
-  const Title = styled.h2`
-    display: block;
-    font-size: 1.5em;
-    margin-block-start: 0.83em;
-    margin-block-end: 0.83em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    font-weight: bold;
-    unicode-bidi: isolate;
-  `;
-
-  const MoreView = styled.div`
-    font-family: '해피니스 산스';
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    padding: 15px 20px 10px;
-  `;
-
-  const EventInner = styled.div`
-    width: 100%;
-    max-width: 960px;
-    height: -moz-fit-content;
-    height: fit-content;
-    padding-left: 20px;
-  `;
-
-  const PopupBannerList = styled.div`
-    width: fit-content !important;
-    height: 100%;
-    position: relative;
-    cursor: pointer;
-  `;
-
-  const FavoriteP = styled.p`
-    display: block;
-    margin-block-start: 1em;
-    margin-block-end: 1em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    unicode-bidi: isolate;
-  `;
-
-  const FavoriteA = styled.a`
-    position: absolute;
-    color: inherit;
-    cursor: pointer;
-    right: 0;
-    top: 0;
-  `;
-
-  const FavoriteImg = styled.img`
-    overflow-clip-margin: content-box;
-    overflow: clip;
-    display: block;
-  `;
-
-  const SlideImgWrap = styled.div`
-    display: block;
-    unicode-bidi: isolate;
-    box-sizing: border-box;
-
-    &:after {
-      background: url(${afterImage}) 0 0;
-      content: ' ';
-      display: block;
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      background-size: cover;
-      left: 0;
-      top: 0;
-      opacity: 0.8;
-      z-index: 1;
-    }
-  `;
-
-  const EventImage = styled.img`
-    width: 260px;
-    height: 260px;
-    border-radius: 8px;
-  `;
-
-  const EventInfo = styled.ul`
-    position: absolute;
-    left: -25px;
-    bottom: 20px;
-    z-index: 10;
-    color: white;
-  `;
-
-  const EventLi = styled.li`
-    padding-bottom: 5px;
-    list-style: none;
-  `;
-
-  const EventName = styled(EventLi)`
-    font-size: 16px;
-    font-weight: 700;
-  `;
-
-  const EventBranch = styled(EventLi)`
-    font-size: 12px;
-    opacity: 0.6;
-    font-weight: 600;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    column-gap: 3px;
-  `;
-
-  const EventType = styled(EventLi)`
-    font-size: 10px;
-    opacity: 0.6;
-    font-weight: 600;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    column-gap: 3px;
-  `;
-
   return (
     <BannerWrap>
       <Header>
@@ -213,28 +231,34 @@ function EventBanner({ title, type }) {
 
       <EventInner>
         <Slider {...settings}>
-          <PopupBannerList>
-            {/* <FontAwesomeIcon icon={faRegularHeart} /> */}
-            <FavoriteP>
-              <FavoriteA>
-                {/* <FontAwesomeIcon icon={faHeart} /> */}
-                <FavoriteImg src={favoriteImage} />
-                {/* <FontAwesomeIcon icon={faHeart} /> */}
-              </FavoriteA>
-            </FavoriteP>
-            <SlideImgWrap>
-              <EventImage src={image} />
-            </SlideImgWrap>
-            <EventInfo>
-              <EventName>
-                <p>Event.getTitle</p>
-              </EventName>
-              <EventBranch>
-                <FontAwesomeIcon icon={faLocationDot} /> Event.getBranchName
-              </EventBranch>
-              <EventType>더현대 서울 </EventType>
-            </EventInfo>
-          </PopupBannerList>
+          {data.map((event) => (
+            // <div className="popupbanner-list">
+
+            // </div>
+            // PopupBannerList
+            <PopupBannerList>
+              {/* <FontAwesomeIcon icon={faRegularHeart} /> */}
+              <FavoriteP>
+                <FavoriteA>
+                  {/* <FontAwesomeIcon icon={faHeart} /> */}
+                  <FavoriteImg src={favoriteImage} />
+                  {/* <FontAwesomeIcon icon={faHeart} /> */}
+                </FavoriteA>
+              </FavoriteP>
+              <SlideImgWrap>
+                <EventImage src={event.imageUrl} />
+              </SlideImgWrap>
+              <EventInfo>
+                <EventName>
+                  <p>{event.title}</p>
+                </EventName>
+                <EventBranch>
+                  <FontAwesomeIcon icon={faLocationDot} /> {event.branchName}
+                </EventBranch>
+                <EventType>{event.eventTypeName} </EventType>
+              </EventInfo>
+            </PopupBannerList>
+          ))}
         </Slider>
       </EventInner>
 
