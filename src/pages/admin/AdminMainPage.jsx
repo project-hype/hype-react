1168ad797aa3cacbf5e4591bbbbdab2f6fc7406a
wonjeong lim <wonjeong@gmail.com>
2024-06-21 00:58:48 +0900

    Feat: Admin 페이지 접근제한 기능 수정

--- src/pages/admin/AdminMainPage.jsx
+++ src/pages/admin/AdminMainPage.jsx
@@ -3,21 +3,14 @@ import AdminMainLayout from '../../layout/admin/AdminMainLayout';
 import { useRecoilValue } from 'recoil';
 import { useNavigate } from 'react-router-dom';
 import { userState } from '../../state/authState';
-import Modal from '../../components/common/Modal';
 
 function AdminMainPage() {
   const user = useRecoilValue(userState);
   const navigate = useNavigate();
-  const [showModal, setShowModal] = useState(false);
-
-  const handleConfirm = () => {
-    setShowModal(false);
-    navigate('/'); // 모달 닫힌 후 메인 페이지로 리다이렉트
-  };
 
   useEffect(() => {
     if (!user.isAdmin) {
-      setShowModal(true);
+      return navigate('/');
     }
   }, [user.isAdmin]);
 
@@ -25,12 +18,7 @@ function AdminMainPage() {
     return navigate('/');
   }
 
-  return (
-    <>
-      <AdminMainLayout />
-      {showModal && <Modal message={'올바르지 않은 접근 권한입니다.'} onConfirm={handleConfirm} />}
-    </>
-  );
+  return <AdminMainLayout />;
 }
 
 export default AdminMainPage;
