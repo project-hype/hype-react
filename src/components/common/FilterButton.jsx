989a41833ada0652f070c019a3474950853293c3
YeongWook <ehdrnr16whduddnr@naver.com>
2024-06-20 23:04:45 +0900

    Feat: 검색 페이지-이벤트 필터 추가

--- src/components/common/FilterButton.jsx
+++ src/components/common/FilterButton.jsx
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
 
 export const FilterButton = styled.button`
+  background-color: ${(props) => (props.active ? '#ff8c00' : 'white')};
+  color: ${(props) => (props.active ? 'white' : '#000')};
+  border: ${(props) => (props.active ? 'none' : '1px solid gray')};
   margin-left: 20px;
   width: 112px;
   height: 43px;
   font-family: '해피니스 산스 레귤러';
   font-size: 14px;
-  color: #000000;
-  border: 1px solid gray;
   border-radius: 20px;
-  background-color: #ffffff;
   cursor: pointer;
   &:hover {
-    color: #ffffff;
-    background-color: #ff8c00;
-    border: 1px solid #ff8c00;
+    background-color: ${(props) => (props.active ? '#ff8c00' : '#eee')};
+    color: ${(props) => (props.active ? 'white' : '#000000')};
+    border: ${(props) => (props.active ? 'none' : '1px solid #ccc')};
   }
 `;
--- src/components/event/EventList.jsx
+++ src/components/event/EventList.jsx
@@ -5,6 +5,16 @@ import '../../assets/scss/common.scss';
 import axios from 'axios';
 import { faHeart, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  text-decoration: none; /* 기본 밑줄 제거 */
+  color: inherit; /* 부모의 색상 상속 */
+  &:hover,
+  &:focus {
+    color: inherit; /* hover와 focus 상태에서도 부모의 색상 유지 */
+  }
+`;
 
 const EventList = ({ events, myInfo, setActiveIndex, likeEvent, likeEventSetter }) => {
   const [likeStatus, setLikeStatus] = useState({}); // 즐겨찾기 상태를 저장할 객체
@@ -58,10 +68,10 @@ const EventList = ({ events, myInfo, setActiveIndex, likeEvent, likeEventSetter 
         events.map((event, index) => (
           <li key={index}>
             <div>
-              <Link to={`/event/${event.eventId}`} className="event-img-wrap">
+              <StyledLink to={`/event/${event.eventId}`} className="event-img-wrap">
                 <img src={event.imageUrl} className="event-img" />
-              </Link>
-              <Link to={`/event/${event.eventId}`} style={{ textDecoration: 'none' }}>
+              </StyledLink>
+              <StyledLink to={`/event/${event.eventId}`} style={{ textDecoration: 'none' }}>
                 <ul>
                   <li className="event-name">
                     <p>{event.title}</p>
@@ -76,7 +86,7 @@ const EventList = ({ events, myInfo, setActiveIndex, likeEvent, likeEventSetter 
                     </p>
                   </li>
                 </ul>
-              </Link>
+              </StyledLink>
               <div>
                 <FontAwesomeIcon
                   icon={likeStatus[event.eventId] ? faHeart : faRegularHeart}
